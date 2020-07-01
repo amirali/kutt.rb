aec657278d97d4aa1ecc1112b7f6e480f78dba79
malekmfs <malekmfs@hotmail.com>
2020-07-01 12:09:07 +0430

    used newer Ruby return and hash syntax style | added new count method

--- lib/kutt.rb
+++ lib/kutt.rb
@@ -4,7 +4,8 @@ require 'json'
 class Kutt
   def initialize(apikey)
     @base_url = 'https://kutt.it'
-    @headers = { 'Content-Type' => 'application/json', 'Accept' => 'application/json', 'X-API-Key' => apikey}
+    @headers = { 'Content-Type': 'application/json', 'Accept': 'application/json',
+       'X-API-Key': apikey}
     return true
   end
 
@@ -13,11 +14,11 @@ class Kutt
     payload['target'] =    url
     payload['customurl'] = args[:customurl] if args[:customurl]
     payload['password'] =  args[:password]  if args[:password]
-    payload['reuse'] =     "true"            if args[:reuse] == true
+    payload['reuse'] =     'true'           if args[:reuse] == true
 
-    r = HTTParty.post(@base_url+'/api/url/submit',
-      :headers => @headers,
-      :body => payload.to_json)
+    r = HTTParty.post(@base_url + '/api/url/submit',
+      headers: @headers,
+      body: payload.to_json)
 
     return r.code, r.to_hash
   end
@@ -26,31 +27,36 @@ class Kutt
     url_array = target.split("/")
     id = url_array.last
 
-    payload = {'id' => id}
+    payload = { 'id': id }
 
-    r = HTTParty.post(@base_url+'/api/url/deleteurl',
-      :headers => @headers,
-      :body => payload.to_json)
+    r = HTTParty.post(@base_url + '/api/url/deleteurl', headers: @headers,
+      body: payload.to_json)
 
     return r.code, r.to_hash
   end
 
   def stats(target)
-    url_array = target.split("/")
+    url_array = target.split('/')
     id = url_array.last
 
-    payload = {'id' => id}
+    payload = {'id': id}
 
-    r = HTTParty.get(@base_url+'/api/url/stats',
-      :headers => @headers,
-      :query => payload)
+    r = HTTParty.get(@base_url + '/api/url/stats',
+      headers: @headers,
+      query: payload)
 
     return r.code, r.to_hash
   end
 
+  def count
+    r = HTTParty.get(@base_url + '/api/url/geturls?count=0',
+      headers: @headers)['countAll']
+      return r
+  end
+
   def list(count = 5)
-    r = HTTParty.get(@base_url+'/api/url/geturls?count='+count.to_s,
-      :headers => @headers)
+    r = HTTParty.get(@base_url + '/api/url/geturls?count=' + count.to_s,
+      headers: @headers)
 
     return r.code, r.to_hash
   end
